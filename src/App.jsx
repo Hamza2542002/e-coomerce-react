dac7e6d36160c7fc9e1258916d528bb24db93aac
Hamza Mosaad <hamza_31056248@f-eng.tanta.edu.eg>
2025-04-15 13:13:28 +0200

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero/Hero'
 import Category from './components/Category/Category'
+import Footer from './components/Footer/Footer'
 export default function App() {
   return (
     <div className='bg-white dark:bg-gray-900'>
       <Navbar />
       <Hero />
       <Category />
+      <Footer />
     </div>
   )
 }
