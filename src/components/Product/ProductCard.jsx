f2e543fba88f985c5e1bc49996137ded61e60e8a
Hamza Mosaad <hamza_31056248@f-eng.tanta.edu.eg>
2025-04-16 19:24:30 +0200

    Fiing Images

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -4,7 +4,7 @@ export default function ProductCard({image , name , price , delay}) {
   return (
     <div data-aos-delay={delay} data-aos="fade-up"  className="">
       <div className='relative group flex justify-center items-center mb-5 rounded-xl bg-gray-200 dark:bg-gray-50 bg-opacity-50 p-5 aspect-[1/1]'>
-        <img src="../../public/HEADPHONE.png" alt="Product" className="w-3/4 h-3/4 group-hover:blur-sm" />
+        <img src="../../src/assets/HEADPHONE.png" alt="Product" className="w-3/4 h-3/4 group-hover:blur-sm" />
         <div className='transition-all duration-300 w-full h-full rounded-[inherit] flex justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-transparent hover:bg-gray-100 hover:bg-opacity-20'>
           <button className="text-white py-2 px-10 invisible group-hover:visible rounded-full bg-primary hover:scale-105 transition-transform duration-300">Buy Now</button>
         </div>
